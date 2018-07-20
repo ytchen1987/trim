44bb63d635fdf879a1b693c2b31d967ac064219d
YT <pbmat@foxmail.com>
2018-07-20 11:45:42 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,19 +3,19 @@
 //删除字符左右空白
 
 function trim(str){
-  return str.replace(/(^\S*)|(\S*$)/g,'');
+  return str.replace(/(^\s*)|(\s*$)/g,'');
 }
 
 
 //删除字符左空白
 
 function ltrim(str){
-  return str.replace(/(^\S*)/g,'');
+  return str.replace(/(^\s*)/g,'');
 }
 
 
 //删除字符右空白
 
 function rtrim(str){
-  return str.replace((\S*$)/g,'');
+  return str.replace((\s*$)/g,'');
 }
